99619cd07df11edb9b80b2699b07072455f3bca3
Kavirajar B <68324620+data-overflow@users.noreply.github.com>
2025-01-14 11:17:19 +0530

    Delete .vercel/output directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/archive-2024/test/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import "clsx";
-function _page($$payload) {
-  $$payload.out += `<p class="text-purple-600">hiiii</p>`;
-}
-export {
-  _page as default
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DTOSqL-F.js","_app/immutable/chunks/disclose-version.C0ON5NiS.js","_app/immutable/chunks/runtime.CdhxA4nL.js","_app/immutable/chunks/Footer.C7q8gNRi.js","_app/immutable/chunks/legacy.D3CvSoT7.js","_app/immutable/chunks/render.CL8mvRJp.js","_app/immutable/chunks/attributes.8jeP3YW2.js","_app/immutable/chunks/Footer.svelte_svelte_type_style_lang.BFfM1Rlb.js","_app/immutable/chunks/index.De_KEhe_.js","_app/immutable/chunks/lifecycle.BSsLOHF7.js","_app/immutable/chunks/store.DyFkm4wv.js","_app/immutable/chunks/index-client.BXlL_vRa.js"];
-export const stylesheets = ["_app/immutable/assets/0.BwBVP95E.css","_app/immutable/assets/Footer.B0FZX9Y1.css"];
-export const fonts = ["_app/immutable/assets/PerfectDOSVGA437.DdISv4LV.ttf","_app/immutable/assets/apple.DwSSc6XR.ttf","_app/immutable/assets/upheavtt.B-9OJ9eJ.ttf"];
